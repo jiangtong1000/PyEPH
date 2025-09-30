5c916bc459f083e09e5a0138f6358bb2a6bee57d
jiangtong1000 <tongjiang@g.harvard.edu>
2025-09-30 13:52:59 -0500

    add qcpbc optimizer

--- pyeph/post_qe2pert/wannier_phonon.py
+++ pyeph/post_qe2pert/wannier_phonon.py
@@ -8,7 +8,6 @@ import jax.scipy.linalg as jsp_linalg
 import numpy, h5py, scipy
 from pyeph.utils.grid import generate_half_qgrids, rgrid_2d_full
 from pyeph.lib.setup_jax import configure_jax_backend
-configure_jax_backend(verbose=True)
 
 # Things to improve:
 # 1. Proskrutes / Parallel transport
@@ -131,6 +130,7 @@ def minimize_wannier_spread(
     q_hbz,
     q_minus,
     partner_hbz_for_minus,
+    fname,
     learning_rate=0.05,
     max_iter=200,
     tol=1e-9,
@@ -152,6 +152,8 @@ def minimize_wannier_spread(
         gauge_opt: optimized unitary gauge matrices of shape (N_q, N_modes, N_modes).
         spread_history: 1D array tracking the spread after each iteration.
     """
+
+    configure_jax_backend(verbose=True)
 
     eigvecs = jnp.asarray(eigenvectors, dtype=jnp.complex128)
     q_vecs = jnp.asarray(q_vectors, dtype=jnp.float64)
@@ -198,7 +200,7 @@ def minimize_wannier_spread(
         loss_real = float(jnp.real(loss))
         loss_track.append(loss_real)
         rho_track.append(rho)
-        with h5py.File("wannier_phonon.h5", "a") as f:
+        with h5py.File(fname, "a") as f:
             if 'loss' not in f:
                 f['loss'] = loss_track
             if 'rho' not in f:
@@ -211,7 +213,7 @@ def minimize_wannier_spread(
         if loss_real < best_loss:
             best_loss = loss_real
             best_params = params
-            with h5py.File("wannier_phonon.h5", "a") as f:
+            with h5py.File(fname, "a") as f:
                 if 'params' not in f:
                     f['params'] = best_params
                 else:
@@ -222,8 +224,175 @@ def minimize_wannier_spread(
             break
         prev_loss = loss_real
         grad = 0.5 * (grad - jnp.swapaxes(grad.conj(), -1, -2))
-        params = params - learning_rate * grad
+        params = params + learning_rate * grad
 
     gauge_opt = _params_to_unitary(best_params, U_base_gauge, partner_hbz_for_minus)
     loss_track = jnp.asarray(loss_track, dtype=jnp.float64)
+    return gauge_opt, loss_track, rho_track
+
+def minimize_wannier_spread_pyqcpbc(
+    eigenvectors,
+    q_vectors,
+    delta_r_vectors,
+    masses,
+    q_hbz,
+    q_minus,
+    partner_hbz_for_minus,
+    max_iter=200,
+    tol=1e-9,
+    initial_params=None,
+    algorithm="l_bfgs_ls",
+    solver_options = None,
+):
+    """Minimize the spread using pyqcpbc's optimizer infrastructure.
+
+    Args:
+        eigenvectors: (N_q, N_modes, N_modes) phonon eigenvectors.
+        q_vectors: (N_q, 3) Brillouin-zone sampling points.
+        delta_r_vectors: (N_R, 3) lattice vectors used for the Wannier transform.
+        masses: (N_atoms,) atomic masses.
+        q_hbz: list of q-points in the half Brillouin zone.
+        q_minus: list of time-reversed partners.
+        partner_hbz_for_minus: mapping from indices in ``q_minus`` to ``q_hbz``.
+        max_iter: maximum number of optimizer iterations.
+        tol: tolerance passed to the pyqcpbc minimizer.
+        initial_params: optional complex array for the initial gauge parameters.
+        algorithm: pyqcpbc optimization algorithm name.
+        solver_options: optional dict of extra ``solver.cfg`` options.
+
+    Returns:
+        gauge_opt: optimized unitary gauge matrices of shape (N_q, N_modes, N_modes).
+        loss_track: 1D array tracking the spread whenever the objective is evaluated.
+        rho_track: list of probability densities corresponding to ``loss_track``.
+    """
+    
+    from typing import Any, Dict, List, Optional
+    from pyqcpbc.OPT import objective_function as qcpbc_objf
+    from pyqcpbc.OPT import minimizer as qcpbc_minimizer
+
+    eigvecs = jnp.asarray(eigenvectors, dtype=jnp.complex128)
+    q_vecs = jnp.asarray(q_vectors, dtype=jnp.float64)
+    delta_r = jnp.asarray(delta_r_vectors, dtype=jnp.float64)
+    masses = jnp.asarray(masses, dtype=jnp.float64)
+    partner_hbz_for_minus = jnp.asarray(partner_hbz_for_minus, dtype=jnp.int32)
+    assert_trs(eigenvectors, q_hbz, q_minus, partner_hbz_for_minus)
+
+    n_modes = eigvecs.shape[-1]
+    U_base_gauge = base_gauge_with_proskrutes(eigenvectors[: len(q_hbz)], q_hbz)
+
+    if initial_params is None:
+        params0 = numpy.zeros((len(q_hbz), n_modes, n_modes), dtype=numpy.complex128)
+    else:
+        params0 = jnp.asarray(initial_params, dtype=jnp.complex128)
+        if params0.shape != (len(q_hbz), n_modes, n_modes):
+            raise ValueError("initial_params must have shape (N_q, N_modes, N_modes)")
+        params0 = numpy.asarray(params0)
+
+    total_size = params0.size
+    params_shape = params0.shape
+
+    def _pack_complex(matrix):
+        arr = numpy.asarray(matrix)
+        return numpy.concatenate([arr.real.ravel(), arr.imag.ravel()])
+
+    def _unpack_complex(vector):
+        half = vector.size // 2
+        real = vector[:half].reshape(params_shape)
+        imag = vector[half:].reshape(params_shape)
+        return real + 1j * imag
+
+    def objective(param_array):
+        gauge = _params_to_unitary(param_array, U_base_gauge, partner_hbz_for_minus)
+        rho = compute_wannier_amplitudes(
+            eigvecs,
+            q_vecs,
+            delta_r,
+            masses,
+            gauge,
+        )
+        loss = compute_marzari_vanderbilt_spread(delta_r, rho)
+        return loss, rho
+
+    value_and_grad = jax.jit(jax.value_and_grad(objective, has_aux=True))
+
+    loss_history = []
+    rho_history = []
+
+    class WannierSpreadObjective(qcpbc_objf):
+        def __init__(self, initial_params_complex):
+            super().__init__(2 * total_size)
+            self.params_vec = _pack_complex(initial_params_complex)
+            self._origin_vec = self.params_vec.copy()
+            self._best_loss = numpy.inf
+            self._best_params_vec = self.params_vec.copy()
+            self._loss = None
+            self._grad_vec = None
+            self._dirty = True
+
+        def _evaluate(self):
+            params_complex = jnp.asarray(_unpack_complex(self.params_vec), dtype=jnp.complex128)
+            (loss, rho), grad = value_and_grad(params_complex)
+            loss_real = float(jnp.real(loss))
+            grad = 0.5 * (grad - jnp.swapaxes(grad.conj(), -1, -2))
+            grad_vec = _pack_complex(grad)
+            self._loss = loss_real
+            self._grad_vec = grad_vec
+            self._dirty = False
+            loss_history.append(loss_real)
+            rho_history.append(numpy.asarray(rho))
+            if loss_real < self._best_loss:
+                self._best_loss = loss_real
+                self._best_params_vec = self.params_vec.copy()
+
+        def _ensure_evaluated(self):
+            if self._dirty or self._loss is None:
+                self._evaluate()
+
+        def get_value(self):
+            self._ensure_evaluated()
+            return self._loss
+
+        def grad(self):
+            self._ensure_evaluated()
+            return self._grad_vec
+
+        def precond(self, x, shift):
+            return x
+
+        def update_params(self, step):
+            self.params_vec = self.params_vec + step.ravel()
+            self._dirty = True
+
+        def save_new_origin(self):
+            self._origin_vec = self.params_vec.copy()
+
+        def back_to_origin(self):
+            self.params_vec = self._origin_vec.copy()
+            self._dirty = True
+
+        @property
+        def best_params_vec(self):
+            return self._best_params_vec
+
+        @property
+        def best_loss(self):
+            return self._best_loss
+
+    objective_wrapper = WannierSpreadObjective(params0)
+    _ = objective_wrapper.get_value()
+
+    solver = qcpbc_minimizer()
+    solver.cfg("algorithm", algorithm)
+    solver.cfg("maxiter", int(max_iter))
+    solver.cfg("tol", float(tol))
+    if solver_options:
+        for key, value in solver_options.items():
+            solver.cfg(key, value)
+
+    solver.run(objective_wrapper)
+
+    best_params_complex = _unpack_complex(objective_wrapper.best_params_vec)
+    gauge_opt = _params_to_unitary(best_params_complex, U_base_gauge, partner_hbz_for_minus)
+    loss_track = jnp.asarray(loss_history, dtype=jnp.float64)
+    rho_track = [jnp.asarray(rho) for rho in rho_history]
     return gauge_opt, loss_track, rho_track
